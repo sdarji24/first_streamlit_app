08c148466eb3a682d507702b5434e472cc936e92
sdarji24 <85360482+sdarji24@users.noreply.github.com>
2022-08-22 09:40:11 -0500

    Update streamlit_app.py
    changed text input to text as text input gives an input typing box

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.subheader('Breakfast Menu')
-streamlit.text_input(' Omega 3 & Blueberry Oatmeal')
-streamlit.text_input(' Kale, Spinach & Rocket Smoothie')
-streamlit.text_input(' Hard-Boiled Free-Range Egg')
+streamlit.text(' Omega 3 & Blueberry Oatmeal')
+streamlit.text(' Kale, Spinach & Rocket Smoothie')
+streamlit.text(' Hard-Boiled Free-Range Egg')
