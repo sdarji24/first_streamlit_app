bd78f25be1c08407ee044bb5c6c938baf0827310
sdarji24 <85360482+sdarji24@users.noreply.github.com>
2022-08-22 09:39:15 -0500

    Update streamlit_app.py
    added subheader 
    added text single line for menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.subheader('Breakfast Menu')
+streamlit.text_input(' Omega 3 & Blueberry Oatmeal')
+streamlit.text_input(' Kale, Spinach & Rocket Smoothie')
+streamlit.text_input(' Hard-Boiled Free-Range Egg')
