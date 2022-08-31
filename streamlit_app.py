ebe31e45d13e5fd3234209ef288c16bcc3783910
sdarji24 <85360482+sdarji24@users.noreply.github.com>
2022-08-30 21:48:05 -0500

    Update streamlit_app.py
    added header and json tag to display api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+
 import requests 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermaelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
